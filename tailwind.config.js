2715901d63bc2215404c680a23efccb0e0cf8324
Krzysztof Dabrowski <10003141@ergo.com>
2024-06-14 13:17:25 +0200

    about and offert

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'main': ['Poppins', 'sans-serif'],
-        'heading': ['Noto Serif', 'sans-serif'],
+        main: ["Poppins", "sans-serif"],
+        heading: ["Playfair Display", "serif"],
       },
       colors: {
-        'primary': {
-          '100': '#faf0e6',
-          '200': '#fff0db',
-          '300': '#eed9c4',
-          '400': '#e4d5b7',
-          '500': '#d9b99b',
+        primary: {
+          100: "#faf0e6",
+          200: "#fff0db",
+          300: "#eed9c4",
+          400: "#e4d5b7",
+          500: "#d9b99b",
         },
       },
     },
   },
   variants: {},
   plugins: [],
-}
+};
